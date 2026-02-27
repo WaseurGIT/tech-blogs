218701f542deb5932341c2854a14f5dbbd4c413b
WaseurGIT <waseurrahman31@gmail.com>
2026-02-27 22:34:49 +0600

    deploying in vercel

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://tech-blog-server-theta.vercel.app/",
 });
 
 axiosSecure.interceptors.request.use((config) => {
