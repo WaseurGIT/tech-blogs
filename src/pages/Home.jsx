fdc3d80ac0f7f3b6c6a7d678947e3d42da8a20d9
WaseurGIT <waseurrahman31@gmail.com>
2026-02-28 15:29:56 +0600

    remove banner and blog images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-import Banner from "../components/Banner";
 import Blogs from "./Blogs";
 
 const Home = () => {
   return (
     <div>
-      <Banner />
       <Blogs />
     </div>
   );
