99aa4edb5803db3a5d3be0a0b7a5003e276cb828
WaseurGIT <waseurrahman31@gmail.com>
2026-02-28 14:48:58 +0600

    deploying in vercel

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "https://tech-blog-server-0qnc.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosSecure.interceptors.request.use((config) => {
